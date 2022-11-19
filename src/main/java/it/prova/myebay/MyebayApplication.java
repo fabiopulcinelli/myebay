0eeb4d0378facd9acbb16d3402f0184fc06c6d86
fabiopulcinelli <fabiopulcinelli2k2@gmail.com>
2022-11-19 15:43:19 +0100

    aggiunta repository e service di utente e ruolo

--- src/main/java/it/prova/myebay/MyebayApplication.java
+++ src/main/java/it/prova/myebay/MyebayApplication.java
@@ -1,13 +1,21 @@
 package it.prova.myebay;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyebayApplication {
+public class MyebayApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyebayApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
